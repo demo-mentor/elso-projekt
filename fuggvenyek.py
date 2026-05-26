7b011981d93cffda2efeb60cf54722d31fbefd01
demo-mentor <demo-mentor@cloudmentor.hu>
2026-05-26 16:32:03 +0200

    Remove unnecessary blank line in fuggveny2 function

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -4,7 +4,6 @@ def fuggveny1():
 def fuggveny2(uzenet):
     print("Ez a második függvény, az üzenet: " + uzenet)
 
-
 def fuggveny3(szam1, szam2):
     eredmeny = szam1 + szam2
     print("Ez a harmadik függvény, az eredmény: " + str(eredmeny))
